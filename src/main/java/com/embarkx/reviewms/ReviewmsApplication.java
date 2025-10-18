a7359fb4f3cdd4c03affe26b7fa910b3e9920c99
Harshal nalawade <harshalnalawade2330@gmail.com>
2025-10-18 22:31:11 +0530

    changes of reviewms service

--- src/main/java/com/embarkx/reviewms/ReviewmsApplication.java
+++ src/main/java/com/embarkx/reviewms/ReviewmsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReviewmsApplication {
+	// Reviewms
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewmsApplication.class, args);
